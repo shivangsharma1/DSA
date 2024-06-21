16eeec03146bcbe4681391b2cf821eb290a45694
shivangsharma1 <shivi97sharma@gmail.com>
2024-06-21 14:30:09 +0530

    leetcode 230

---
+++ tree/Kth_smallest_element_bst_recursive.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def __init__(self):
+        self.out = []
+    
+    def inorder(self,root):  
+        if not root: return None
+        
+        self.inorder(root.left)
+        self.out.append(root.val)
+        self.inorder(root.right)
+    
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        if not root: return None
+        self.inorder(root)
+        return self.out[k-1]
+        
