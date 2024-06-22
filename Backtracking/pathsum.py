6fd813021d87fac9e83d9bb694504669623d54d0
shivangsharma1 <shivi97sharma@gmail.com>
2024-06-22 11:51:33 +0530

    leetcode 112

---
+++ Backtracking/pathsum.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+        def dfs(node, cursum):
+            if not node:
+                return False
+            
+            cursum+=node.val
+            if not node.left and not node.right and cursum == targetSum:
+                return True
+            else: False
+
+            return (dfs(node.left, cursum) or dfs(node.right, cursum))
+
+        return dfs(root, 0)
+        
