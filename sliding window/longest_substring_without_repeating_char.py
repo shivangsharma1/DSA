251a3465874376c97b89f78a6dcddacad573efdb
shivangsharma1 <shivi97sharma@gmail.com>
2024-07-25 09:45:23 +0530

    logic update

--- sliding window/longest_substring_without_repeating_char.py
+++ sliding window/longest_substring_without_repeating_char.py
@@ -1,20 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left, right = 0, 0
         maxlen = 0
+        left = 0
         hashset = set()
-        # hashset.add(s[left])
-        while right<len(s):
-            if s[right] not in hashset:
-                hashset.add(s[right])
-                curlen = right-left+1
-                maxlen = max(maxlen, curlen)
-                right+=1
-
-            else:
+        for right in range(len(s)):
+            while s[right] in hashset:
                 hashset.remove(s[left])
                 left+=1
-                right+=1
+            hashset.add(s[right])
+            maxlen = max(maxlen, right-left+1)
 
         return maxlen
 
@@ -24,16 +18,3 @@ obj.lengthOfLongestSubstring("pwwkew")
 
 
 
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         window_len = 0
-#         left = 0
-#         chars = set()
-#         for right in range(len(s)):
-#             while s[right] in chars:
-#                 chars.remove(s[left])
-#                 left+=1
-#             chars.add(s[right])
-#             window_len = max(window_len, right-left+1)
-
-#         return window_len
