2381d04d82394a1fb248d79df289a327f56fd020
shivangsharma1 <shivi97sharma@gmail.com>
2024-08-04 20:13:39 +0530

    daily temp problem

---
+++ stack/daily_temperature.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        l=[0]*len(temperatures)
+        stack=[]
+        for i,val in enumerate(temperatures):
+            while stack and temperatures[stack[-1]]<val:
+                j=stack.pop()
+                l[j]=i-j
+                
+            stack.append(i)
+        return l
+            
+
+obj = Solution()
+examples = [73,74,75,71,69,72,76,73]
+print(obj.dailyTemperatures(examples))
